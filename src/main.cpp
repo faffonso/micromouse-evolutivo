cde7b8b2c07561a683b30876235f6344e6792a1d
Felipe-Tommaselli <f.tommaselli@usp.br>
2021-12-27 11:53:54 -0300

    Começo do código

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,67 @@
 #include <string>
 #include <exception>
 
+#define TAMPOP 10
+
+unsigned char crom[TAMPOP];
+float fit[TAMPOP];
+int generation = 0;
+
+// TODO: Criar o labirinto (montar a matriz e o labirinto com as paredes 0 e os espaços livres 1)
+// no começo podemos só usar um labiritno exemplo - TOMAS
+int MazeCreation(){
+    
+}
+
+// TODO: Fazer a movimentação dos indivíduos dentro do labirinto
+// cada individuo vai ter um cromossomo com os movimentos deles, essa funçaõ deve fazer essa mov.
+// ela que vai dizer em que bloco que ele parou até bater em uma parede ou até chegar no destino
+// sugestão: se ele bateu na nº possição do cromossomo, da n-ésima + 1 pra frente deixar tudo 5
+// sugestão: se ele chegou na soluçao na nº posição, da n-ésima pra frente deixar tudo 6 - TOMAS
+int MoveinMaze(){
+
+}
+
+// TODO: Iniciar população 
+// GIAN
+int InitPopulation(){
+    
+}
+
+// TODO: Criar função de avaliação 
+// TOMAS
+int FitnessFunction(){
+    
+}
+
+// TODO: Tipo de avaliação que será feita para escolher os melhores indíviduos, no nosso caso é interessante utilizar uma que seja mais elitista
+// incluindo a predação - GIAN
+int Selection(){
+
+}
+
+// TODO: Crossover dos cromossomos, lembrando que os do melhor indíviduos são "misturados" com o dos outros
+// GIAN
+int Crossover(){
+
+}
+
+// TODO: Mutação dos indíviduos a partir do crossover feito anteriormente
+// GIAN
+int Mutation(){
+
+}
+
+// TODO: Rearranja população mutada para o início de uma nova geração 
+// GIAN 
+int RearrangePop(){
+
+}
+
 
 int main(){
+    
+    
     std::cout << "hello world\n";
     return 0;
 }
