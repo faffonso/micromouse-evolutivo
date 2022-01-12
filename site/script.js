35ff6aaad55cb73cfd62f615f9e8e418c5963c24
Felipe-Tommaselli <f.tommaselli@usp.br>
2022-01-11 22:20:04 -0300

    odeio JS papo reto

--- site/script.js
+++ site/script.js
@@ -97,7 +97,7 @@ function maze (value) {
         const date = Date.now();
         let currentDate = null;
         do {
-          currentDate = Date.now();
+            currentDate = Date.now();
         } while (currentDate - date < milliseconds);
     
     }
@@ -187,3 +187,110 @@ function render() {
     ctx.restore();
 }
 
+//! PARTE DO FILE
+
+/*
+function getdata(){
+    fetch('test.txt')
+    .then(response => response.text())
+    .then(text => {
+        const array = text.split("\n");
+        console.log(array); // print?
+    })
+}
+
+function handleFiles(input){
+
+    const file = input.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+        const file = event.target.result;
+        const allLines = file.split(/\r\n|\n/);
+        // Reading line by line
+        allLines.forEach((line) => {
+            console.log(line);
+        });
+    };
+
+    reader.onerror = (event) => {
+        alert(event.target.error.name);
+    };
+
+    reader.readAsText(file);
+}
+
+function readSingleFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        displayContents(contents);
+    };
+    reader.readAsText(file);
+    }
+
+    function displayContents(contents) {
+    var element = document.getElementById('file-content');
+    element.textContent = contents;
+    }
+
+    document.getElementById('file-input')
+    .addEventListener('change', readSingleFile, false);
+
+    */
+document.getElementById('file').onchange = function(){
+
+    var file = this.files[0];
+    
+    var reader = new FileReader();
+    reader.onload = function(progressEvent){
+        // Entire file
+        console.log(this.result);
+    
+        // By lines
+        var lines = this.result.split('\n');
+        for(var line = 0; line < lines.length; line++){
+        console.log(lines[line]);
+        }
+    };
+    reader.readAsText(file);
+    };
+
+/*
+
+var numsList = [];
+$.ajax( 'arquivo.txt', {
+    dataType: 'text',
+    success: function(response){
+        //response é o conteudo do arquivo.txt
+        var lines = response.split('\n'); //quebra o arquivo em linhas, 
+        for(var i in lines){
+            var row = lines[i];
+            var nums = row.split(','); //quebra a linha em valores separdos por virgula
+            for(var j in nums){
+                var num = parseInt(nums[j]); //converte o valor para int
+                if( !isNaN(num) ) //basicamente verifica se é um numero
+                    numsList.push(num); //adiciona o item no array
+            }
+        }
+
+        console.log(numsList);
+    }
+});
+*/
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        var res = this.responseText;
+        alert(res); //  A Variável res terá o conteúdo.
+        }
+    };
+    xhttp.open("GET", "test.txt", true);
+    xhttp.send();
+}
